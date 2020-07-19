5b8955bedf7ebe3a1877e0fa98193e7eeb107d7d
paparocketeer <paparocketeer@gmail.com>
2020-07-19 15:47:44 +0300

    finish

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+
+import axios from 'axios'
+const dynamicRoutes = () => {
+  const routes = axios
+    .get('https://admin.rocketjump.top/wp-json/wp/v2/posts?page=1&per_page=20')
+    .then((res) => {
+      return res.data.map(post => `/blog/${post.slug}`)
+    })
+  console.log(routes)
+  return routes
+}
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'static',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Alata|Open+Sans&display=swap'
+      }
+    ]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: ['~/assets/mixins.scss'],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    '~/plugins/posts.server.js',
+    '~/plugins/tags.server.js',
+    '~/plugins/dateformat.js'
+  ],
+  generate: {
+    routes: dynamicRoutes
+  },
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {},
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+    extend (config, ctx) {}
+  }
+}
---
+++ store/index.js
@@ -0,0 +1,66 @@
+const siteURL = 'https://admin.rocketjump.top'
+
+export const state = () => ({
+  posts: [],
+  tags: []
+})
+
+export const mutations = {
+  updatePosts: (state, posts) => {
+    state.posts = posts
+  },
+  updateTags: (state, tags) => {
+    state.tags = tags
+  }
+}
+
+export const actions = {
+  async getPosts ({ state, commit, dispatch }) {
+    if (state.posts.length) { return }
+
+    try {
+      let posts = await fetch(
+        `${siteURL}/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
+      ).then(res => res.json())
+
+      posts = posts
+        .filter(el => el.status === 'publish')
+        .map(({ id, slug, title, excerpt, date, tags, content }) => ({
+          id,
+          slug,
+          title,
+          excerpt,
+          date,
+          tags,
+          content
+        }))
+
+      commit('updatePosts', posts)
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  async getTags ({ state, commit }) {
+    if (state.tags.length) { return }
+
+    let allTags = state.posts.reduce((acc, item) => {
+      return acc.concat(item.tags)
+    }, [])
+    allTags = allTags.join()
+
+    try {
+      let tags = await fetch(
+        `${siteURL}/wp-json/wp/v2/tags?page=1&per_page=40&include=${allTags}`
+      ).then(res => res.json())
+
+      tags = tags.map(({ id, name }) => ({
+        id,
+        name
+      }))
+
+      commit('updateTags', tags)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
